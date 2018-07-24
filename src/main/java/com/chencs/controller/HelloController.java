be112fffb901ea62ea5c2cddaca0b768f9c719da
chencs0603 <chencs0603@gmail.com>
2018-07-24 10:57:35 +0800

    整理代码

--- src/main/java/com/chencs/controller/HelloController.java
+++ src/main/java/com/chencs/controller/HelloController.java
@@ -3,7 +3,6 @@ package com.chencs.controller;
 import com.chencs.aspect.ControllerLog;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
